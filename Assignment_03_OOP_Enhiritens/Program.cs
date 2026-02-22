c99c014ef94c11e1e02b7e45074da2eaea7d8327
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-02-22 17:16:30 +0200

    Add project files.

---
+++ Assignment_03_OOP_Enhiritens/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_03_OOP_Enhiritens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
